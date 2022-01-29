b378aa2e2fd1a6e8327f810e73bc17a6f935a944
Brett Fullam <brettfullam@redBox.local>
2022-01-29 09:00:41 -0300

    IOC Generator v2.0

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # IOC-Generator-v2
 Quickly generate common Indicators of Compromise (IOCs) from files with Python.
+
+### Quickly generate common Indicators of Compromise (IOCs) from files with Python.
+
+An analyst can quickly generate common Indicators of Compromise (IOCs) from a single file or directory that can then be incorporated into tools like Mandiant's IOCe.
+
+``` noLineNumbers
+usage: ioc-generator-v2.0.py [-h] [-f FILE] [-d DIRECTORY] [-V]
+
+Python Automated IOC Generator v2.0 by Brett Fullam
+
+options:
+  -h, --help                show this help message and exit
+  -f FILE, --file FILE      select file as input
+  -d DIRECTORY, --directory DIRECTORY
+                            select directory as input
+  -V, --version             show program version
+```
+
+The file is processed and each of the IOCs are generated and output to the screen, as well as to a text file called "output.txt" which is located in the same directory the python script resides.
+
+``` noLineNumbers
+File name: 2innocent.pdf
+
+
+MD5: 2942bfabb3d05332b66eb128e0842cff
+
+SHA-1: 90ffd2359008d82298821d16b21778c5c39aec36
+
+SHA-256: 3df79d34abbca99308e79cb94461c1893582604d68329a41fd4bec1885e6adb4
+
+Size in bytes:  13264
+
+
+IOCs generated on: Sat Jan 29 08:41:22 2022
+created by Python Automated IOC Generator
+```
+
+For more information about this script, check out my article on "[Security Automation with Python — Quickly generate common IOCs from files with Python](https://www.brettfullam.com/security-automation-with-python-quickly-generate-common-io-cs-from-files-with-python  "Quickly generate common IOCs from files with Python")." 
+
+## Getting Started
+
+1. Download the script or use git to clone the repository
+
+2. No extra dependencies needed.  This script was created using Python3, and all of the necessary dependencies are already included in the standard Python installation.
+
+3. The repository includes the following:
+
+* The finished version of the script using only using standard Python libraries (file name ending in ".py")
+* License
+* README.md file
+* 2innocent.pdf sample file for testing
+* ioc-samples directory that contains 2 sample files for testing
+
+``` nolinenumbers
+.
+├── 2innocent.pdf
+├── LICENSE
+├── README.md
+├── ioc-generator-v2.0.py
+└── ioc-samples
+
+1 directory, 4 files
+```
+The directory, which contains 2innocent.pdf and highly_malicious.txt, are harmless sample files for you to test the "directory_as_input" functionality included in the script to see first hand how multiple files are processed.
---
+++ ioc-generator-v2.0.py
@@ -0,0 +1,161 @@
+# Include standard modules
+import argparse
+import time
+from pathlib import Path
+import hashlib
+
+# //////////////////////////////////////////////
+#
+# Python Automated IOC Generator v2.0
+#
+# File Name, MD5 hash value,
+# SHA-1 hash value, SHA-256 hash value,
+# and File Size in Bytes.
+#
+# //////////////////////////////////////////////
+
+# Initiate the parser
+parser = argparse.ArgumentParser(description="Python Automated IOC Generator v2.0 by Brett Fullam")
+parser.add_argument("-f", "--file", help="select file as input")
+parser.add_argument("-d", "--directory", help="select directory as input")
+parser.add_argument("-V", "--version", help="show program version", action="store_true")
+
+
+#/////////////  BEGIN -- timestamp
+
+# grab the epoch timestamp at run time and convert to human-readable for the artifact output document footer information
+timeStamp = time.time()
+
+# convert epoch timestamp to human-readable date time formatted
+report_time = time.strftime('%c', time.localtime(timeStamp))
+
+# create a custom string to be included at the end of the generated output
+report_time_footer = str('IOCs generated on: ') + report_time + str('\ncreated by Python Automated IOC Generator') + str('\n\n')
+
+#/////////////  END -- timestamp
+
+
+#/////////////  BEGIN -- Generate IOCs
+#///////////// File Name, MD5 hash, SHA-1 hash, SHA-256 hash, File size in bytes
+
+# create and open a file named 'output.txt' to write our data to
+f = open("output.txt", "w")
+
+def iocGrab(arg):
+
+    # store a single entry value from direct user input, or from the directory_as_input() function
+    target_file = arg
+
+    # use Path module to access .stat results -- 'name' to grab the file name, and 'st_size' to grab the file size in bytes    
+    file_ = Path(target_file)
+    fileName = (file_.name)
+    fileStats = (file_.stat().st_size)
+
+    # Grab the name of the file, create a custom string, print output to screen, as well as write to 'output.txt'
+    fileNameOutput = "\nFile name: " + fileName + "\n\n"
+    print(fileNameOutput)
+    f.write(fileNameOutput)
+
+    # /// Hashing START
+
+    # Open and read the file contents to create the MD5 hash of the file
+    md5_hash = hashlib.md5()
+    with open(target_file,"rb") as f4:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f4.read(4096),b""):
+            md5_hash.update(byte_block)
+        md5hash = (md5_hash.hexdigest())
+
+    # Output the MD5 hash value created by hashlib.md5() as reference
+    md5Data = str("MD5: " + md5hash + "\n")
+    print(md5Data)
+    f.write(md5Data)
+
+
+    # Open and read the file contents to create the SHA-1 hash of the file
+    sha1_hash = hashlib.sha1()
+    with open(target_file,"rb") as f3:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f3.read(4096),b""):
+            sha1_hash.update(byte_block)
+        sha1hash = (sha1_hash.hexdigest())
+
+    # Output the SHA-1 hash value created by hashlib.sha1() as reference
+    sha1Data = str("SHA-1: " + sha1hash + "\n")
+    print(sha1Data)
+    f.write(sha1Data)
+
+
+    # Open and read the file contents to create the SHA-256 hash of the file
+    sha256_hash = hashlib.sha256()
+    with open(target_file,"rb") as f2:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f2.read(4096),b""):
+            sha256_hash.update(byte_block)
+        sha256hash = (sha256_hash.hexdigest())
+
+    # Output the SHA-256 hash value created by hashlib.sha256() as reference
+    sha256Data = str("SHA-256: " + sha256hash + "\n")
+    print(sha256Data)
+    f.write(sha256Data)
+
+    # /// Hashing END
+
+    # Create a custom string to show the file size in bytes
+    fileSizeBytes = ("Size in bytes:  " + str(fileStats) + ("\n\n"))
+
+    # Store the 'size' value as fileSizeBytes, create a custom string, print output to screen, as well as write to 'output.txt'
+    print(fileSizeBytes)
+    f.write(fileSizeBytes)
+
+    return
+
+#/////////////  END -- Generate IOCs
+#///////////// File Name, MD5 hash, SHA-1 hash, SHA-256 hash, File size in bytes
+
+#/////////////  BEGIN -- Directory as input
+
+def directory_as_input(arg):
+
+    # store the directory_as_input value in the path_of_the_directory variable
+    path_of_the_directory = arg
+
+    # initialize list 'i'
+    i = [ ]
+
+    # use Path to get the file paths for each file in the directory
+    entries = Path(path_of_the_directory)
+    for i in entries.iterdir():
+        # send a concatenated value using "i.parent" and "i.name" to create the relative path for each file
+        # which will appears as "directory-indicated/filename" as the loop iterates over the list 'i"
+        grabPath = (i.parent / i.name)
+        # the 'grabPath' value is then passed to the iocGrab() function
+        iocGrab(grabPath)
+
+    f.write(report_time_footer)
+    f.close()
+
+    print(report_time_footer)
+
+#/////////////  END -- Directory as input
+
+
+# Read arguments from the command line
+args = parser.parse_args()
+
+
+# Check for --version or -V
+if args.file:
+    iocGrab(args.file)
+    print(report_time_footer)
+    f.write(report_time_footer)
+    f.close()
+# Check for --directory or -d
+elif args.directory:
+    directory_as_input(args.directory)
+# Check for --file or -f
+elif args.version:
+    print("IOC Generator version 2.0")
+# Print usage information if no arguments are provided
+else:
+    print("usage: ioc-generator-v2.0.py [-h] [-f FILE] [-d DIRECTORY] [-V]")
